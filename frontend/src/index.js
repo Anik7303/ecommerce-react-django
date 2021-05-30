20c68a4959b33ae9a9d3f21b4870442738046865
Anik7303 <anik7303@gmail.com>
2021-05-30 22:07:04 +0600

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './bootstrap.min.css'
+import './index.css'
+import App from './App'
+
+const element = (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
