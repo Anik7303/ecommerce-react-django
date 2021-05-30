faaef0ad201fbe8cc70bf4d71449909314112a8a
Anik7303 <anik7303@gmail.com>
2021-05-30 23:01:06 +0600

    Frontend styling complete

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,66 +1,32 @@
+import { useCallback } from 'react'
+const fullStar = 'fas fa-star'
+const halfStar = 'fas fa-star-half-alt'
+const star = 'far fa-star'
+
 function Rating({ value, text, color }) {
+    const calculateRating = useCallback(() => {
+        const rating = []
+        let i = 0.5,
+            j = 1
+        for (let k = 0; k < 5; k++) {
+            if (value >= j + k) {
+                rating.push(fullStar)
+            } else if (value >= i + k) {
+                rating.push(halfStar)
+            } else {
+                rating.push(star)
+            }
+        }
+        return rating
+    }, [value])
+
     return (
         <div className="rating">
-            <span>
-                <i
-                    style={{ color }}
-                    className={
-                        value >= 1
-                            ? 'fas fa-star'
-                            : value >= 0.5
-                            ? 'fas fa-star-half-alt'
-                            : 'far fa-star'
-                    }
-                ></i>
-            </span>
-            <span>
-                <i
-                    style={{ color }}
-                    className={
-                        value >= 2
-                            ? 'fas fa-star'
-                            : value >= 1.5
-                            ? 'fas fa-star-half-alt'
-                            : 'far fa-star'
-                    }
-                ></i>
-            </span>
-            <span>
-                <i
-                    style={{ color }}
-                    className={
-                        value >= 3
-                            ? 'fas fa-star'
-                            : value >= 2.5
-                            ? 'fas fa-star-half-alt'
-                            : 'far fa-star'
-                    }
-                ></i>
-            </span>
-            <span>
-                <i
-                    style={{ color }}
-                    className={
-                        value >= 4
-                            ? 'fas fa-star'
-                            : value >= 3.5
-                            ? 'fas fa-star-half-alt'
-                            : 'far fa-star'
-                    }
-                ></i>
-            </span>
-            <span>
-                <i
-                    style={{ color }}
-                    className={
-                        value >= 5
-                            ? 'fas fa-star'
-                            : value >= 4.5
-                            ? 'fas fa-star-half-alt'
-                            : 'far fa-star'
-                    }
-                ></i>
-            </span>
+            {calculateRating().map((item, index) => (
+                <span key={index}>
+                    <i style={{ color }} className={item}></i>
+                </span>
+            ))}
             <span>{text && text}</span>
         </div>
     )
--- frontend/src/pages/ProductScreen.js
+++ frontend/src/pages/ProductScreen.js
@@ -1,7 +1,90 @@
-import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+
+import products from '../products'
+import Rating from '../components/Rating'
 
 function ProductScreen() {
-    return <div>Product</div>
+    const { id } = useParams()
+    const {
+        name,
+        image,
+        rating,
+        numReviews,
+        price,
+        description,
+        countInStock,
+    } = products.find((p) => p._id === id)
+
+    const isInStock = countInStock > 0
+
+    return (
+        <div>
+            <Link to="/" className="btn btn-light my-3">
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={image} alt={name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            {
+                                <Rating
+                                    value={rating}
+                                    text={`${numReviews} reviews`}
+                                    color="#f8e825"
+                                />
+                            }
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${price}</ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        <strong>
+                                            {isInStock
+                                                ? 'In Stock'
+                                                : 'Out of Stock'}
+                                        </strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className="col-12"
+                                    type="button"
+                                    disabled={!isInStock}
+                                >
+                                    Add to Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
 }
 
 export default ProductScreen
